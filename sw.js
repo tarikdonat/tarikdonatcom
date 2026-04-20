c1751b8d32af0d13c7e31dddbcdba65a71d88f76
tarikdonat <161738046+tarikdonat@users.noreply.github.com>
2026-04-20 16:40:51 +0300

    Uygulanan Tasarım, PWA ve SEO iyileştirmeleri

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'tarikdonat-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/vitrin.html',
+  '/news.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
